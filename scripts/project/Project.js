3e719af6cee884998bd524d4c2380a7c663f0b59
Chelsea Ash <crash1115@yahoo.com>
2025-04-22 01:25:33 -0400

    Update project data model

--- scripts/project/Project.js
+++ scripts/project/Project.js
@@ -2,13 +2,16 @@ export default class Project {
     constructor(actor){
         this.id = foundry.utils.randomID();
         this.owner = actor.id;
+        this.sharedWith = []; // Currently unused. Planned feature coming soon.
         this.name = "New Project";
         this.img = "icons/commodities/tech/blueprint.webp";
-        this.note = "";
+        this.category = undefined; // Currently unused. Planned feature coming soon.
         this.progress = {
             current: 0,
             max: 1,
             label: ""
-        }
+        };
+        this.note = "";
+        this.gmOnly = false; // Currently unused. Planned feature coming soon.
     }
 }
