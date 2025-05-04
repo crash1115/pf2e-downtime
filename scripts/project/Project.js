4fdd97917d41d81a4895ef23f7e87e62434481e1
Chelsea Ash <crash1115@yahoo.com>
2025-05-04 13:10:43 -0400

    Add gmOnly and infoOnly fields to Project data model

--- scripts/project/Project.js
+++ scripts/project/Project.js
@@ -2,16 +2,16 @@ export default class Project {
     constructor(actor){
         this.id = foundry.utils.randomID();
         this.owner = actor.id;
-        this.sharedWith = []; // Currently unused. Planned feature coming soon.
         this.name = "New Project";
         this.img = "icons/commodities/tech/blueprint.webp";
-        this.category = undefined; // Currently unused. Planned feature coming soon.
+        this.category = undefined;
         this.progress = {
             current: 0,
             max: 1,
             label: ""
         };
         this.note = "";
-        this.gmOnly = false; // Currently unused. Planned feature coming soon.
+        this.gmOnly = false;
+        this.infoOnly = false;
     }
 }
